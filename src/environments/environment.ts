534243e9b6466475ecedfa6fbeb1e9d3aa353d7e
Erick Mendelski <erick.mendelski@gmail.com>
2026-02-25 16:44:56 -0300

    chore(environment): update API base URL to use 3000 port

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
-  apiBaseUrl: 'http://todolist-api.test/api/v1',
+  // apiBaseUrl: 'http://todolist-api.test/api/v1',
+  apiBaseUrl: 'http://localhost:3000/api/v1', //
 };
 
